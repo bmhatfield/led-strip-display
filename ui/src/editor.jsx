da5c347777f85c93182cb304b5c876fabd666d68
Brian Hatfield <bmhatfield@gmail.com>
2017-10-29 11:02:53 -0400

    Greatly clarify the intent of the editor save method

--- ui/src/editor.jsx
+++ ui/src/editor.jsx
@@ -1,5 +1,22 @@
 class Editor extends React.Component {
     save() {
+        /*
+            The actual physical LED strip is a 1px by 150px display. For the purposes
+            of this editor, it will be wrapped around a window frame, creating a 39x33
+            marquee that encircles the window.
+
+            The service side accepts pixel values in strip order, which does not exactly
+            match the ordering of the HTML elements on the page. This means we need to
+            reorder slightly so when we send the values to the service, we are sending
+            them in clockwise order, starting from the bottom right (as viewed from *outside*
+            the window)
+
+            To accomplish this, we'll find the elements matching each side separately, which
+            will allow us to order the 'sides' in clockwise order. It will also allow us to
+            order the pixels within a given side as appropriate so that the total flow is
+            clockwise.
+        */
+
         // Get our App's Editor component
         let editor = document.getElementById('editor')
 
@@ -15,18 +32,25 @@ class Editor extends React.Component {
         let topPixels = Array.from(topPixelsCollection);
         let rightPixels = Array.from(rightPixelsCollection);
 
-        // Pixels are generated left to right, but physically rendered right-to-left.
+        /*
+            The HTML elements for each 'side' are generated left-to-right.
+            This means that two of the sides will be 'wrong' when considered
+            for the purposes of clockwise order, so we need to reverse their
+            ordering so the final array of pixels represents the continuous
+            clockwise ordering of pixels. The result will be that the display
+            matches the UI.
+        */
         bottomPixels.reverse();
-
-        // Pixels are generated top to bottom, but physically rendered bottom-to-top.
         leftPixels.reverse();
 
-        // Actually represents the side render-ordering of the strip
+        // We can now represent the total ordering of HTML elements in a nested array
         let sides = [bottomPixels, leftPixels, topPixels, rightPixels];
 
-        // Will represent the pixel render-ordering of the strip
+        // And this will be the actual array ordering of RGB values to send to the
+        // backend service.
         let strip = [];
 
+        // Iterate over the two-level array, extracting RGB values for each pixel.
         for (let side of sides) {
             for (let pixel of side) {
                 strip.push(pixel.style.backgroundColor);
