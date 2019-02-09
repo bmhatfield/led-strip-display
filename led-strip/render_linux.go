0d1d9adfbf386ee68e2d0dc2c32d369bfdbbc4c7
Brian Hatfield <bmhatfield@gmail.com>
2019-02-09 13:57:42 -0500

    Update WS2011 library

--- led-strip/render_linux.go
+++ led-strip/render_linux.go
@@ -2,24 +2,45 @@ package ledstrip
 
 import (
 	"github.com/bmhatfield/led-strip-display/frame"
-	ws2811 "github.com/jgarff/rpi_ws281x" // Will not build on OSX
+	ws2811 "github.com/rpi-ws281x/rpi-ws281x-go"
 )
 
 // LinuxStrip to satisfy the Strip interface
-type LinuxStrip struct{}
+type LinuxStrip struct {
+	device *ws2811.WS2811
+}
 
 // Init initializes the ws2811 strip
 func (s LinuxStrip) Init(gpio, pixels, brightness int) error {
-	return ws2811.Init(gpio, pixels, brightness)
+	options := ws2811.DefaultOptions
+
+	options.Channels[0].GpioPin = gpio
+	options.Channels[0].LedCount = pixels
+	options.Channels[0].Brightness = brightness
+
+	device, err := ws2811.MakeWS2811(&options)
+
+	if err != nil {
+		return err
+	}
+
+	s.device = device
+	return s.device.Init()
 }
 
 // Render publishes 150 pixels to an LED strip
 func (s LinuxStrip) Render(strip frame.HexGRB) error {
-	for index, color := range strip {
-		ws2811.SetLed(index, color)
+	err := s.device.Wait()
+	if err != nil {
+		return err
 	}
 
-	return ws2811.Render()
+	leds := s.device.Leds(0)
+	for index, color := range strip {
+		leds[index] = color
+	}
+
+	return s.device.Render()
 }
 
 // GetStrip returns a LinuxStrip to satisfy the Strip interface
